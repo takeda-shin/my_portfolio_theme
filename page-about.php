fdd7dc5127afae9df47d60d036e0093a4d7e4cce
takeda-shin <kgcfy958@yahoo.co.jp>
2020-04-28 22:18:46 +0900

    page-about

---
+++ page-about.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+  <section class="contents">
+    <h2 class="heading">ABOUT</h2>
+    <div class="about-cal about-cal-image">
+      <div class="about-icon">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/image.jpg" , class="icon-img" , alt="顔イメージ" , height=250px , width=250px>
+      </div>
+    </div>
+    <div class="about-cal about-cal-info">
+      <ul class="about-info">
+        <li class="about-info-title">名前</li>
+        <li class="about-info-content" >武田 慎也（Shinya Takeda）</li>
+        <li class="about-info-title">誕生</li>
+        <li class="about-info-content" >1989年4月29日（31歳）</li>
+        <li class="about-info-title">拠点</li>
+        <li class="about-info-content" >福岡市博多区</li>
+        <li class="about-info-title">学歴</li>
+        <li class="about-info-content" >長崎大学大学院　水産環境科学総合研究科</li>
+        <li class="about-info-title">略歴</li>
+        <li class="about-info-content" >5年半勤務した葬儀屋さんでIT関連業務を経験。その中でプログラミングの魅力に取り憑かれる。『TECH:CAMP』福岡校オープンが後押しとなり、エンジニア転職を決意する。</li>
+        <li class="about-info-title">開発経験</li>
+        <li class="about-info-content" >HTML(Haml)・CSS(SCSS)・Ruby・Ruby on Rails・PHP・Laravel・JavaScript・jQuery・GitHub・AWS等の技術を用いた複合的機能の企画・実装</li>
+        <li class="about-info-title">その他経験</li>
+        <li class="about-info-content" >・Webサービス立上げ・運営<br>・パンフレット・チラシデザインおよびディレクション<br>・システム開発における要件定義</li>
+      </ul>
+    </div>
+  </section>
+
+<?php get_footer(); ?>
