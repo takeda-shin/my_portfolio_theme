91f1894437cfd8a5854064720afa923583cb656b
takeda-shin <kgcfy958@yahoo.co.jp>
2020-05-05 23:36:31 +0900

    Create page-blog.php

---
+++ page-blog.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+  <section class="posts-contents">
+    <h2 class="heading">BLOG</h2>
+    <div class="posts">
+      <div class="post">
+        <div class="post-header">
+          <div class="post-image">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/image.jpg", height=360px , width=640px>
+          </div>
+          <div class="post-meta">
+            2020年5月5日【カテゴリ】
+          </div>
+          <h2 class="post-title">
+            <a href="#">タイトル</a>
+          </h2>
+          <div class="post-body">
+            <p>本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sidebar">
+      <div class="widget">
+      sidebar
+    </div>
+
+  </section>
+
+<?php get_footer(); ?>
